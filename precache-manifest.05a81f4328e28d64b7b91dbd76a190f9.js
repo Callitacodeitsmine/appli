09667e28d7682159c645b3cef688206d89d3647d
callitacodeitsmine <107363591+Callitacodeitsmine@users.noreply.github.com>
2023-05-08 02:08:01 +0530

    Updates

---
+++ precache-manifest.05a81f4328e28d64b7b91dbd76a190f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "710db053e6cc82ff5608bfd75fb6ee00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "effc182857fb08ace2d3",
+    "url": "/static/js/2.38e075bd.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/static/js/2.38e075bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49b618442d99ea5bb1ad",
+    "url": "/static/js/main.2113efed.chunk.js"
+  },
+  {
+    "revision": "ef09c47ebed553faf789",
+    "url": "/static/js/runtime-main.9d7701dd.js"
+  }
+]);
